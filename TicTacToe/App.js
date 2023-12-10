14b009ba6963c573b0de744f87cc69725b995fd6
mrfoott <phuvo2105@gmail.com>
2023-12-10 19:58:20 +0700

    Update for Tic Tac Toe game

--- TicTacToe/App.js
+++ TicTacToe/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+// App.js
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, Button, TextInput } from 'react-native';
 import Board from './components/Board';
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [boardSizeInput, setBoardSizeInput] = useState('3');
+  const [boardSize, setBoardSize] = useState(3);
+  const [squares, setSquares] = useState(Array(boardSize * boardSize).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    setSquares(Array(boardSize * boardSize).fill(null));
+    setXIsNext(true);
+    setWinner(null);
+  }, [boardSize]);
 
   const handlePress = (index) => {
     const newSquares = [...squares];
@@ -19,20 +28,36 @@ const App = () => {
   };
 
   const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+    const lines = [];
+    for (let i = 0; i < boardSize; i++) {
+      // Kiểm tra hàng ngang
+      for (let j = 0; j < boardSize - 2; j++) {
+        lines.push(Array.from({ length: 3 }, (_, index) => i * boardSize + j + index));
+      }
+      // Kiểm tra hàng dọc
+      for (let j = 0; j < boardSize - 2; j++) {
+        lines.push(Array.from({ length: 3 }, (_, index) => i + (j + index) * boardSize));
+      }
+      // Kiểm tra 3 ô giống nhau liên tiếp theo đường chéo chính
+      if (i <= boardSize - 3) {
+        for (let j = 0; j < boardSize - 2; j++) {
+          lines.push(Array.from({ length: 3 }, (_, index) => (i + index) * (boardSize + 1) + j + index));
+        }
+      }
+      // Kiểm tra 3 ô giống nhau liên tiếp theo đường chéo phụ
+      if (i <= boardSize - 3) {
+        for (let j = 0; j < boardSize - 2; j++) {
+          lines.push(Array.from({ length: 3 }, (_, index) => (i + index) * (boardSize - 1) + (boardSize - 1 - j) - index));
+        }
+      }
+    }
+
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+      const line = lines[i];
+      const symbols = line.map((index) => squares[index]);
+      const [firstSymbol] = symbols;
+      if (symbols.every((symbol) => symbol === firstSymbol && symbol !== null)) {
+        return firstSymbol;
       }
     }
     if (squares.every((square) => square !== null)) {
@@ -42,14 +67,20 @@ const App = () => {
   };
 
   const resetGame = () => {
-    setSquares(Array(9).fill(null));
+    setSquares(Array(boardSize * boardSize).fill(null));
     setXIsNext(true);
     setWinner(null);
   };
 
-  const getRandomColor = () => {
-    const colors = ['#FF5733', '#33FF7A', '#336CFF', '#FF33E9', '#E9FF33'];
-    return colors[Math.floor(Math.random() * colors.length)];
+  const handleSizeChange = (text) => {
+    setBoardSizeInput(text);
+  };
+
+  const handleSizeSubmit = () => {
+    const size = parseInt(boardSizeInput);
+    if (!isNaN(size) && size >= 3 && size <= 5) {
+      setBoardSize(size);
+    }
   };
 
   let status;
@@ -75,10 +106,18 @@ const App = () => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Chọn kích thước bạn muốn chơi (3-5)</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleSizeChange}
+        onSubmitEditing={handleSizeSubmit}
+        keyboardType="numeric"
+        value={boardSizeInput}
+      />
       <Text style={[styles.playerText, { color: '#FF4136' }]}>Người chơi 1: X</Text>
       <Text style={[styles.playerText, { color: '#33FF7A' }]}>Người chơi 2: O</Text>
       {status}
-      <Board squares={squares} onPress={handlePress} />
+      <Board squares={squares} onPress={handlePress} boardSize={boardSize} />
       {(winner === 'draw' || winner) && (
         <View style={styles.buttonContainer}>
           <Button title="Chơi lại" onPress={resetGame} />
@@ -93,6 +132,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    width: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 5,
   },
   status: {
     marginBottom: 10,
--- TicTacToe/components/Board.js
+++ TicTacToe/components/Board.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Square from './Square';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
 
-const Board = ({ squares, onPress }) => {
-  return (
-    <View style={styles.board}>
-      {squares.map((value, index) => (
-        <Square key={index} value={value} onPress={() => onPress(index)} />
-      ))}
-    </View>
-  );
+const Board = ({ squares, onPress, boardSize }) => {
+  const renderSquare = (index) => {
+    let textColor = '#000'; // Màu chữ mặc định là đen
+    if (squares[index] === 'X') {
+      textColor = '#FF5733'; // Màu đỏ cho X
+    } else if (squares[index] === 'O') {
+      textColor = '#33FF7A'; // Màu xanh lá cho O
+    }
+
+    return (
+      <TouchableOpacity key={index} style={styles.square} onPress={() => onPress(index)}>
+        <Text style={[styles.text, { color: textColor }]}>{squares[index]}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderBoard = () => {
+    const board = [];
+    for (let i = 0; i < boardSize; i++) {
+      const row = [];
+      for (let j = 0; j < boardSize; j++) {
+        row.push(renderSquare(i * boardSize + j));
+      }
+      board.push(<View key={i} style={styles.boardRow}>{row}</View>);
+    }
+    return board;
+  };
+
+  return <View>{renderBoard()}</View>;
 };
 
 const styles = StyleSheet.create({
-  board: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+  square: {
+    width: 50,
+    height: 50,
+    borderWidth: 1,
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boardRow: {
+    flexDirection: 'row',
   },
 });
 
